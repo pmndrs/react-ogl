2a1d2b238d4ec50e5da5b50c242f9c216f5c4237
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-11-06 21:38:19 -0500

    chore: create demo

---
+++ demo/src/index.jsx
@@ -0,0 +1,33 @@
+import * as OGL from 'ogl'
+import { useRef } from 'react'
+import { render } from 'react-dom'
+import { useFrame, Canvas } from 'react-ogl/web'
+import vertex from './shaders/vertex'
+import fragment from './shaders/fragment'
+
+const Box = (props) => {
+  const mesh = useRef()
+
+  useFrame(() => (mesh.current.rotation.x += 0.01))
+
+  return (
+    <mesh {...props} ref={mesh}>
+      <box />
+      <program
+        vertex={vertex}
+        fragment={fragment}
+        uniforms={{
+          uColor: { value: new OGL.Color(0xf5ce54) },
+        }}
+      />
+    </mesh>
+  )
+}
+
+render(
+  <Canvas camera={{ position: [0, 0, 8] }}>
+    <Box position={[-1.2, 0, 0]} />
+    <Box position={[1.2, 0, 0]} />
+  </Canvas>,
+  document.getElementById('root'),
+)
---
+++ demo/vite.config.js
@@ -0,0 +1,15 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      react: path.resolve('./node_modules/react'),
+      'react-dom': path.resolve('./node_modules/react-dom'),
+      'react-ogl': path.resolve('../src'),
+      'react-ogl/web': path.resolve('../src/web'),
+    },
+  },
+  plugins: [react()],
+})
