5c06308f861199b33dc02b0bc25dcff858e22d55
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-11-08 02:11:46 -0600

    chore(test): cleanup

--- src/test/render.js
+++ src/test/render.js
@@ -1,4 +1,3 @@
-import { createRoot } from '../'
 import { createInternals } from '../shared'
 
 /**
@@ -8,13 +7,11 @@ export const render = (element, config = {}) => {
   // Create canvas shim
   const canvas = config.canvas || document.createElement('canvas')
 
-  // Init state
-  const internalState = createInternals(canvas, config)
+  // Init internals
+  const { root } = createInternals(canvas, config)
 
   // Render and get output state
-  let state = {}
-  if (!state.root) state.root = createRoot(canvas, internalState)
-  state = state.root.render(element)
+  const state = root.render(element)
 
   return state
 }
