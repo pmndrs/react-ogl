1cfbf6812eeb3e38f8b659f573b5d15b21b192ca
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-11-08 05:23:45 -0600

    chore: cleanup build config

--- rollup.config.js
+++ rollup.config.js
@@ -3,43 +3,77 @@ const path = require('path')
 const babel = require('@rollup/plugin-babel').default
 const commonjs = require('@rollup/plugin-commonjs')
 const resolve = require('@rollup/plugin-node-resolve').default
-const { devDependencies, dependencies, peerDependencies } = require('./package.json')
 
-const external = [
-  'react-native/Libraries/Pressability/Pressability',
-  ...Object.keys({ ...devDependencies, ...dependencies, ...peerDependencies }),
-]
+// Filter external dependencies
+const root = process.platform === 'win32' ? path.resolve('/') : '/'
+const external = (id) => !id.startsWith('.') && !id.startsWith(root)
+
+// Configure plugins
 const plugins = [
   commonjs(),
   babel({ babelHelpers: 'bundled' }),
   resolve({ extensions: ['.js', '.jsx'] }),
+  {
+    name: 'patch-targets',
+    generateBundle({ file }) {
+      // Create target folders
+      const target = file.replace(/\/index.c?js$/, '')
+      const outputDir = path.resolve(process.cwd(), target)
+      fs.mkdirSync(outputDir, { recursive: true })
+
+      switch (target) {
+        case 'dist': {
+          // Copy files
+          ;['LICENSE', 'README.md', 'package.json'].forEach((file) => {
+            fs.copyFileSync(
+              path.resolve(process.cwd(), file),
+              path.resolve(process.cwd(), 'dist', file),
+            )
+          })
+        }
+        default: {
+          // Create a package.json for each target with main and module resolutions
+          fs.writeFileSync(
+            path.resolve(outputDir, 'package.json'),
+            JSON.stringify(
+              {
+                sideEffects: 'false',
+                main: './index.cjs',
+                module: './index.js',
+              },
+              null,
+              2,
+            ),
+          )
+        }
+      }
+    },
+  },
 ]
 
 // Traverse targets
-const targets = fs
-  .readdirSync(path.resolve(process.cwd(), 'src'))
-  .reduce((acc, fileName) => {
+const targets = fs.readdirSync(path.resolve(process.cwd(), 'src')).reduce(
+  (acc, fileName) => {
     const isFolder = !fileName.includes('.')
-    if (isFolder) acc.push(fileName)
+    if (isFolder) acc.push(`${fileName}/index`)
 
     return acc
-  }, [])
+  },
+  ['index'],
+)
 
 // Export targets
-module.exports = ['index', ...targets.map((target) => `${target}/index`)].flatMap(
-  (target) => [
-    {
-      input: `./src/${target}.js`,
-      output: { file: `dist/${target}.js`, format: 'esm' },
-      external,
-      plugins,
-    },
-    {
-      input: `./src/${target}.js`,
-      output: { file: `dist/${target}.cjs`, format: 'cjs' },
-      external,
-      plugins,
-    },
-  ],
-)
-module.exports.targets = targets
+module.exports = targets.flatMap((target) => [
+  {
+    input: `./src/${target}.js`,
+    output: { file: `dist/${target}.js`, format: 'esm' },
+    external,
+    plugins,
+  },
+  {
+    input: `./src/${target}.js`,
+    output: { file: `dist/${target}.cjs`, format: 'cjs' },
+    external,
+    plugins,
+  },
+])
