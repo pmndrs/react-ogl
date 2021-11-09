c3f5a86581bbada371a3f625401dd96e0d1599c5
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-11-09 07:00:27 -0600

    chore: remove ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   transform: {
     '^.+\\.(mjs|jsx?)$': 'babel-jest',
-    '^.+\\.tsx$': 'ts-jest',
   },
   transformIgnorePatterns: ['node_modules/(?!ogl)'],
-  testMatch: ['<rootDir>/tests/**/*.test.{js,jsx,ts,tsx}'],
+  testMatch: ['<rootDir>/tests/**/*.test.{js,jsx}'],
   testEnvironment: 'jsdom',
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleFileExtensions: ['js', 'jsx'],
   verbose: true,
   testTimeout: 30000,
   setupFilesAfterEnv: ['<rootDir>/tests/setupTests.js'],
