8c0bf019c05475dbd8548e0bf8c29c5fa33109ad
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-01-23 07:46:45 -0600

    fix(deps): correctly mark native peer deps

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   resolve: {
     alias: {
+      react: path.resolve('../node_modules/react'),
+      'react-dom': path.resolve('../node_modules/react-dom'),
+      ogl: path.resolve('../node_modules/ogl'),
       'react-ogl': path.resolve('../src'),
     },
   },
