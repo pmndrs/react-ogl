f4449680cde0b05ac0ac22b6173ac51083095ac5
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-03-16 12:58:50 -0500

    chore(tests): complete applyProps coverage

--- tests/utils.test.tsx
+++ tests/utils.test.tsx
@@ -1,8 +1,63 @@
 // @ts-ignore
 import * as OGL from 'ogl'
-import { applyProps } from '../src'
+import { applyProps, Instance } from '../src'
 
 describe('applyProps', () => {
+  it('should accept shorthand uniforms', async () => {
+    const canvas = document.createElement('canvas')
+    const gl = canvas.getContext('webgl2')
+    const program = new OGL.Program(gl, {
+      vertex: '',
+      fragment: '',
+      uniforms: {},
+    })
+
+    applyProps(program, {
+      uniforms: {
+        foo: { value: 0 },
+        bar: 1,
+      },
+    })
+
+    expect(program.uniforms).toMatchSnapshot()
+  })
+
+  it('should convert CSS color names to color uniforms', async () => {
+    const canvas = document.createElement('canvas')
+    const gl = canvas.getContext('webgl2')
+    const program = new OGL.Program(gl, {
+      vertex: '',
+      fragment: '',
+      uniforms: {},
+    })
+
+    applyProps(program, {
+      uniforms: {
+        color: 'red',
+      },
+    })
+
+    expect(program.uniforms).toMatchSnapshot()
+  })
+
+  it('should convert arrays into vector uniforms', async () => {
+    const canvas = document.createElement('canvas')
+    const gl = canvas.getContext('webgl2')
+    const program = new OGL.Program(gl, {
+      vertex: '',
+      fragment: '',
+      uniforms: {},
+    })
+
+    applyProps(program, {
+      uniforms: {
+        uv: [0, 1],
+      },
+    })
+
+    expect(program.uniforms).toMatchSnapshot()
+  })
+
   it('should diff & merge uniforms', async () => {
     const canvas = document.createElement('canvas')
     const gl = canvas.getContext('webgl2')
@@ -22,5 +77,55 @@ describe('applyProps', () => {
     applyProps(program, { uniforms: { c: 2 } })
 
     expect(program.uniforms).toMatchSnapshot()
+  })
+
+  it('should pierce into nested properties', async () => {
+    const canvas = document.createElement('canvas')
+    const gl = canvas.getContext('webgl2')
+    const program = new OGL.Program(gl, {
+      vertex: '',
+      fragment: '',
+      uniforms: {},
+    })
+
+    applyProps(program, {
+      'uniforms-color': 'red',
+    })
+
+    expect(program.uniforms).toMatchSnapshot()
+  })
+
+  it('should prefer to copy from external props', async () => {
+    const target = { color: new OGL.Color() } as unknown as Instance
+    target.color.copy = jest.fn()
+
+    applyProps(target, {
+      color: new OGL.Color(),
+    })
+
+    expect(target.color).toBeInstanceOf(OGL.Color)
+    expect(target.color.copy).toHaveBeenCalled()
+  })
+
+  it('should spread array prop values', async () => {
+    const target = { position: new OGL.Vec3() } as unknown as Instance
+
+    applyProps(target, {
+      position: [1, 2, 3],
+    })
+
+    expect(target.position).toBeInstanceOf(OGL.Vec3)
+    expect(Array.from(target.position)).toMatchSnapshot()
+  })
+
+  it('should accept scalar shorthand', async () => {
+    const target = { position: new OGL.Vec3() } as unknown as Instance
+
+    applyProps(target, {
+      position: 3,
+    })
+
+    expect(target.position).toBeInstanceOf(OGL.Vec3)
+    expect(Array.from(target.position)).toMatchSnapshot()
   })
 })
