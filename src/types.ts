c835bfd3c376e8c15ab06d090e884d39fe491515
Konstantin Timoshenko <rondo.devil@gmail.com>
2022-03-19 14:44:21 +0300

    Fix types for delegator

--- src/types.ts
+++ src/types.ts
@@ -140,7 +140,7 @@ export type DPR = [number, number] | number
 export type RenderProps = {
   size?: { width: number; height: number }
   dpr?: DPR
-  renderer?: OGL.Renderer | Partial<NonFunctionKeys<OGL.Renderer>> | Partial<OGL.RendererOptions>
+  renderer?: (canvas: HTMLCanvasElement) =>  OGL.Renderer | OGL.Renderer | Partial<NonFunctionKeys<OGL.Renderer>> | Partial<OGL.RendererOptions>
   gl?: OGL.OGLRenderingContext
   frameloop?: 'always' | 'never'
   camera?: CameraProps | Partial<NonFunctionKeys<OGL.Camera>> | Partial<OGL.CameraOptions>
