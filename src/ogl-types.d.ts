41aff67ee45831dbe5456fba94141ad1ca7b55ff
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-05-31 07:51:37 -0500

    chore(types): complete return types

--- src/ogl-types.d.ts
+++ src/ogl-types.d.ts
@@ -571,11 +571,11 @@ declare module 'ogl' {
     set r(v: number)
     set g(v: number)
     set b(v: number)
-    set(color: [number, number, number])
-    set(color: number, g: number, b: number)
-    set(color: string)
-    set(color: 'black' | 'white' | 'red' | 'green' | 'blue' | 'fuchsia' | 'cyan' | 'yellow' | 'orange')
-    set(color: number)
+    set(color: [number, number, number]): this
+    set(color: number, g: number, b: number): this
+    set(color: string): this
+    set(color: 'black' | 'white' | 'red' | 'green' | 'blue' | 'fuchsia' | 'cyan' | 'yellow' | 'orange'): this
+    set(color: number): this
     copy(v: Color): this
   }
 
@@ -1030,14 +1030,14 @@ declare module 'ogl' {
     static resolveURI(uri: any, dir: any): string
     static loadBuffers(desc: any, dir: any): Promise<any[]>
     static parseBufferViews(gl: any, desc: any, buffers: any): any
-    static async parseImages(gl: any, desc: any, dir: any, bufferViews: any): Promise<any>
+    static parseImages(gl: any, desc: any, dir: any, bufferViews: any): Promise<any>
     static parseTextures(gl: any, desc: any, images: any): any
     static createTexture(
       gl: any,
       desc: any,
       images: any,
       opts: { sample: any; source: any; name: any; extensions: any; extras: any },
-    )
+    ): any
     static parseMaterials(gl: any, desc: any, textures: any): any
     static parseSkins(gl: any, desc: any, bufferViews: any): any
     static parseMeshes(gl: any, desc: any, bufferViews: any, materials: any, skins: any): any
@@ -1067,7 +1067,7 @@ declare module 'ogl' {
       max: any
     }
     static parseNodes(gl: any, desc: any, meshes: any, skins: any, images: any): any
-    static parseLights(gl: any, desc: any, nodes: any, scenes: any)
+    static parseLights(gl: any, desc: any, nodes: any, scenes: any): any
     static populateSkins(skins: any, nodes: any): void
     static parseAnimations(gl: any, desc: any, nodes: any, bufferViews: any): any
     static parseScenes(desc: any, nodes: any): any
@@ -1231,7 +1231,7 @@ declare module 'ogl' {
     maxDistance: number
     offset: number
     zoomStyle: 'dolly' | any
-    mouseButtons = { ORBIT: number, ZOOM: number, PAN: number }
+    mouseButtons: { ORBIT: number; ZOOM: number; PAN: number }
     target: Vec3
   }
 
