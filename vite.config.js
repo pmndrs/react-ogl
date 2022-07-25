4d898532c9a02cd27a387104bbeb99910199531f
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-07-24 19:12:13 -0500

    fix: enable tree-shaking in webpack, sourcemaps

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import path from 'path'
 import fs from 'fs'
 import { defineConfig } from 'vite'
 
+const NATIVE = fs.existsSync(path.resolve(process.cwd(), 'dist'))
+const entry = NATIVE ? 'index.native' : 'index'
+
 export default defineConfig({
-  root: 'examples',
+  root: process.argv[2] ? undefined : 'examples',
   resolve: {
     alias: {
       'react-ogl': path.resolve(process.cwd(), 'src'),
@@ -11,18 +14,25 @@ export default defineConfig({
   },
   build: {
     minify: false,
-    outDir: path.resolve(process.cwd(), 'dist'),
     emptyOutDir: false,
+    sourcemap: true,
     target: 'es2018',
     lib: {
       formats: ['es'],
-      entry: fs.existsSync(path.resolve(process.cwd(), 'dist'))
-        ? path.resolve(process.cwd(), 'src/index.native.ts')
-        : path.resolve(process.cwd(), 'src/index.ts'),
+      entry: `src/${entry}.ts`,
       fileName: '[name]',
     },
     rollupOptions: {
       external: (id) => !id.startsWith('.') && !path.isAbsolute(id),
+      preserveModules: !NATIVE,
+      sourcemapExcludeSources: true,
     },
   },
+  plugins: [
+    {
+      generateBundle() {
+        this.emitFile({ type: 'asset', fileName: `${entry}.d.ts`, source: `export * from '../src/${entry}'` })
+      },
+    },
+  ],
 })
